393d40225fcd8599e618ee9a53290e5766c8d4b8
Hasnain Mucklai <mucklaihasnain@gmail.com>
2024-09-30 14:43:55 -0700

    Created login or register so user is redirected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import FeaturesPage from './pages/FeaturesPage';
+import LoginRegisterPage from './pages/LoginRegisterPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/features" element={<FeaturesPage />} />
+          <Route path="/login" element={<LoginRegisterPage />} />
         </Routes>
       </div>
     </Router>
