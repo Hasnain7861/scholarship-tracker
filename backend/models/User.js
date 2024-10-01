985636fb9aa3d091d4c2137dd40d202927416950
Hasnain Mucklai <mucklaihasnain@gmail.com>
2024-10-01 13:55:09 -0700

    Removed hashing logic

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -25,22 +24,3 @@ const UserSchema = new mongoose.Schema({
     },
     
 }, { timestamps: true });
-
-// Password hashing middleware
-UserSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (err) {
-        next(err);
-    }
-});
-
-//password comparison method
-UserSchema.methods.matchPassword = async function(enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-};
-
-module.exports = mongoose.model('User', UserSchema);
