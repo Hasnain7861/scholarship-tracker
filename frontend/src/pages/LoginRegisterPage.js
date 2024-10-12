912b0460d715a150eac6c26907fd3cab63bec373
Hasnain Mucklai <mucklaihasnain@gmail.com>
2024-10-11 17:28:11 -0700

    Minor code cleanup

--- frontend/src/pages/LoginRegisterPage.js
+++ frontend/src/pages/LoginRegisterPage.js
@@ -5,7 +5,7 @@ import '../styles/LoginRegisterPage.css';
 const LoginRegisterPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isRegister, setIsRegister] = useState(true); // Toggle between login and register
+  const [isRegister, setIsRegister] = useState(true);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
